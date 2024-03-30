06db7ae84ac248a93bec68afb53c3faf39a00e4f
Eric Xu <xeric.2002@gmail.com>
2024-03-30 15:28:30 -0400

    Simplify redirect for roulette
    Instead of waiting for the page to fully render before executing a
    `useEffect` hook, a `redirect` can be called on the server since the
    logic is so simple (not much waiting time).

--- src/app/roulette/page.tsx
+++ src/app/roulette/page.tsx
@@ -1,26 +1,9 @@
-import Link from "next/link";
 import { type ReactElement } from "react";
-import Random from "./Random";
+import { Route } from "next";
+import { redirect } from "next/navigation";
+import { pickRandom } from "@/lib/utils";
 
-export default function Roulette(): ReactElement {
-  return (
-    <>
-      <Random />
-
-      <div className="flex h-screen min-h-min flex-col">
-        <header className="sticky flex h-12 w-full flex-shrink-0 items-center justify-center border-b border-white/10 shadow-lg">
-          <Link
-            href="/"
-            className="whitespace-nowrap text-center text-lg font-semibold uppercase transition hover:text-white/75 active:scale-95"
-          >
-            Dead by Roulette
-          </Link>
-        </header>
-
-        <main className="p-4">
-          <p>Selecting a role for you, please wait&hellip;</p>
-        </main>
-      </div>
-    </>
-  );
+export default async function Roulette(): Promise<ReactElement> {
+  const routes: Route[] = ["/roulette/killer", "/roulette/survivor"];
+  redirect(pickRandom(routes));
 }
