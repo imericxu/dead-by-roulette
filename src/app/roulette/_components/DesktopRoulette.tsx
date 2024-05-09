e5ca57f37fc5cf3df1613cda558f0b1e276647e0
Eric Xu <xeric.2002@gmail.com>
2024-05-08 23:04:27 -0500

    Add tooltips on hover for desktop roulette
    TODO: Mobile interaction with the desktop site will likely be a tap for
    context menu, which will show the name and an option to randomize one or
    all (if applicable).

--- src/app/roulette/_components/DesktopRoulette.tsx
+++ src/app/roulette/_components/DesktopRoulette.tsx
@@ -5,9 +5,10 @@ import { type Item } from "@/lib/dbd";
 import DbdRole from "@/lib/dbdRole";
 import type Loadout from "@/lib/loadout";
 import { LoadoutPart } from "@/lib/loadout";
+import { Timeout } from "@/lib/utils";
 import rarityBg from "@/lib/variants/rarityBg";
 import { useState, type ReactElement } from "react";
-import { Button } from "react-aria-components";
+import { Button, Tooltip, TooltipTrigger } from "react-aria-components";
 import { twJoin, twMerge } from "tailwind-merge";
 import { match } from "ts-pattern";
 import LoadDesktopRoulette from "./LoadDesktopRoulette";
@@ -29,166 +30,252 @@ export default function DesktopRoulette({
   const [abilityHovered, setAbilityHovered] = useState(false);
   const [abilityPressed, setAbilityPressed] = useState(false);
 
-  const [addOnsHovered, setAddOnsHovered] = useState(false);
-  const [addOnsPressed, setAddOnsPressed] = useState(false);
+  const [addOnsHoveredIdx, setAddOnsHoveredIdx] = useState<number | null>(null);
+  const [addOnsPressedIdx, setAddOnsPressedIdx] = useState<number | null>(null);
 
-  const [perksHovered, setPerksHovered] = useState(false);
-  const [perksPressed, setPerksPressed] = useState(false);
+  const [offeringHovered, setOfferingHovered] = useState(false);
+
+  const [perksHoveredIdx, setPerksHoveredIdx] = useState<number | null>(null);
+  const [perksPressedIdx, setPerksPressedIdx] = useState<number | null>(null);
 
   if (loadout === null) return <LoadDesktopRoulette />;
   return (
     <div className="col-span-full col-start-1 mt-4 hidden w-full max-w-[888px] grid-cols-[auto_min-content_max-content_min-content_max-content] grid-rows-[max-content_min-content_max-content] justify-self-center sm:grid">
       {/* Character */}
-      <Button
-        onPress={() => {
-          randomizeHandler(LoadoutPart.character);
-        }}
-        onHoverChange={setCharacterHovered}
-        onPressChange={setCharacterPressed}
-        className={twMerge(
-          "relative col-start-1 row-span-full h-[225px] w-full justify-self-end border border-main-light bg-gradient-to-b from-orange-950/80 to-stone-950/90 outline-0 transition focus-visible:outline-2 md:h-[257px]",
-          (characterHovered || (role === DbdRole.killer && abilityHovered)) &&
-            "brightness-125",
-          (characterPressed || (role === DbdRole.killer && abilityPressed)) &&
-            "border-main-heavy",
-        )}
-      >
-        <LoadDetectImage
-          src={`${loadout.character.bigImg}-264h.png`}
-          alt=""
-          width={0}
-          height={0}
-          priority
-          className="pointer-events-none m-auto h-full w-auto overflow-hidden object-cover transition data-[loading=true]:opacity-0"
-        />
-      </Button>
-
-      {/* Ability and Add-Ons */}
-      <div className="col-start-3 row-start-1 flex items-end gap-4">
-        {/* Ability */}
+      <TooltipTrigger isOpen={characterHovered} delay={0} closeDelay={0}>
         <Button
           onPress={() => {
-            randomizeHandler(LoadoutPart.ability);
+            randomizeHandler(LoadoutPart.character);
           }}
-          onHoverChange={setAbilityHovered}
-          onPressChange={setAbilityPressed}
-          aria-label={`Randomize ${role === DbdRole.killer ? "power" : "item"}`}
+          onHoverChange={setCharacterHovered}
+          onPressChange={setCharacterPressed}
           className={twMerge(
-            twJoin(
-              "relative h-24 w-24 border border-main-light outline-0 transition focus-visible:outline-2 md:h-28 md:w-28",
-              match(role)
-                .with(
-                  DbdRole.killer,
-                  () => "bg-gradient-to-b from-orange-900 to-orange-950/80",
-                )
-                .with(DbdRole.survivor, () =>
-                  rarityBg((loadout.ability as Item).rarity),
-                )
-                .exhaustive(),
-            ),
-            ((characterHovered && role === DbdRole.killer) || abilityHovered) &&
+            "relative col-start-1 row-span-full h-[225px] w-full justify-self-end border border-main-light bg-gradient-to-b from-orange-950/80 to-stone-950/90 outline-0 transition focus-visible:outline-2 md:h-[257px]",
+            (characterHovered || (role === DbdRole.killer && abilityHovered)) &&
               "brightness-125",
-            ((characterPressed && role === DbdRole.killer) || abilityPressed) &&
+            (characterPressed || (role === DbdRole.killer && abilityPressed)) &&
               "border-main-heavy",
           )}
         >
           <LoadDetectImage
-            src={`${loadout.ability.img}-112w.png`}
+            src={`${loadout.character.bigImg}-264h.png`}
             alt=""
+            width={0}
+            height={0}
             priority
-            fill
-            className="pointer-events-none transition data-[loading=true]:opacity-0"
+            className="pointer-events-none m-auto h-full w-auto overflow-hidden object-cover transition data-[loading=true]:opacity-0"
           />
         </Button>
+        <Tooltip
+          offset={8}
+          placement="bottom"
+          className="border border-main-light bg-black/50 px-4 py-2 backdrop-blur-sm"
+        >
+          {loadout.character.name}
+        </Tooltip>
+      </TooltipTrigger>
+
+      {/* Ability and Add-Ons */}
+      <div className="col-start-3 row-start-1 flex items-end gap-4">
+        {/* Ability */}
+        <TooltipTrigger isOpen={abilityHovered} delay={0} closeDelay={0}>
+          <Button
+            onPress={() => {
+              randomizeHandler(LoadoutPart.ability);
+            }}
+            onHoverChange={setAbilityHovered}
+            onPressChange={setAbilityPressed}
+            aria-label={`Randomize ${
+              role === DbdRole.killer ? "power" : "item"
+            }`}
+            className={twMerge(
+              twJoin(
+                "relative h-24 w-24 border border-main-light outline-0 transition focus-visible:outline-2 md:h-28 md:w-28",
+                match(role)
+                  .with(
+                    DbdRole.killer,
+                    () => "bg-gradient-to-b from-orange-900 to-orange-950/80",
+                  )
+                  .with(DbdRole.survivor, () =>
+                    rarityBg((loadout.ability as Item).rarity),
+                  )
+                  .exhaustive(),
+              ),
+              ((characterHovered && role === DbdRole.killer) ||
+                abilityHovered) &&
+                "brightness-125",
+              ((characterPressed && role === DbdRole.killer) ||
+                abilityPressed) &&
+                "border-main-heavy",
+            )}
+          >
+            <LoadDetectImage
+              src={`${loadout.ability.img}-112w.png`}
+              alt=""
+              priority
+              fill
+              className="pointer-events-none transition data-[loading=true]:opacity-0"
+            />
+          </Button>
+          <Tooltip
+            placement="bottom"
+            offset={8}
+            className="border border-main-light bg-black/50 px-4 py-2 backdrop-blur-sm"
+          >
+            {loadout.ability.name}
+          </Tooltip>
+        </TooltipTrigger>
 
         {/* Add-Ons */}
         <div className="flex shrink-0 gap-2">
-          {loadout.addOns.map((addOn) => (
-            <Button
+          {loadout.addOns.map((addOn, idx) => (
+            <TooltipTrigger
               key={addOn.id}
-              onPress={() => {
-                randomizeHandler(LoadoutPart.addOns);
-              }}
-              onHoverChange={setAddOnsHovered}
-              onPressChange={setAddOnsPressed}
-              className={twMerge(
-                twJoin(
-                  "relative h-20 w-20 border border-main-light outline-0 transition focus-visible:outline-2 md:h-24 md:w-24",
-                  rarityBg(addOn.rarity),
-                ),
-                ((role === DbdRole.killer && characterHovered) ||
-                  abilityHovered ||
-                  addOnsHovered) &&
-                  "brightness-125",
-                ((role === DbdRole.killer && characterPressed) ||
-                  abilityPressed ||
-                  addOnsPressed) &&
-                  "border-main-heavy",
-              )}
+              isOpen={addOnsHoveredIdx === idx}
+              delay={0}
+              closeDelay={0}
             >
-              <LoadDetectImage
-                src={`${addOn.img}-96w.png`}
-                alt=""
-                fill
-                priority
-                className="pointer-events-none transition data-[loading=true]:opacity-0"
-              />
-            </Button>
+              <Button
+                onPress={() => {
+                  randomizeHandler(LoadoutPart.addOns);
+                }}
+                onHoverStart={() => {
+                  setAddOnsHoveredIdx(idx);
+                }}
+                onHoverEnd={() => {
+                  setAddOnsHoveredIdx(null);
+                }}
+                onPressStart={() => {
+                  setAddOnsPressedIdx(idx);
+                }}
+                onPressEnd={() => {
+                  setAddOnsPressedIdx(null);
+                }}
+                className={twMerge(
+                  twJoin(
+                    "relative h-20 w-20 border border-main-light outline-0 transition focus-visible:outline-2 md:h-24 md:w-24",
+                    rarityBg(addOn.rarity),
+                  ),
+                  ((role === DbdRole.killer && characterHovered) ||
+                    abilityHovered ||
+                    addOnsHoveredIdx !== null) &&
+                    "brightness-125",
+                  ((role === DbdRole.killer && characterPressed) ||
+                    abilityPressed ||
+                    addOnsPressedIdx !== null) &&
+                    "border-main-heavy",
+                )}
+              >
+                <LoadDetectImage
+                  src={`${addOn.img}-96w.png`}
+                  alt=""
+                  fill
+                  priority
+                  className="pointer-events-none transition data-[loading=true]:opacity-0"
+                />
+              </Button>
+              <Tooltip
+                placement="bottom"
+                offset={8}
+                className="flex flex-col items-center border border-main-light bg-black/50 px-4 py-2 backdrop-blur-sm"
+              >
+                <span>{loadout.addOns[idx].name}</span>
+                <span className="text-sm opacity-75">
+                  Hold shift to randomize a single add-on
+                </span>
+              </Tooltip>
+            </TooltipTrigger>
           ))}
         </div>
       </div>
 
       {/* Offering */}
-      <Button
-        onPress={() => {
-          randomizeHandler(LoadoutPart.offering);
-        }}
-        className="clip-hexagon col-start-5 row-start-1 h-24 w-[84px] bg-main-light p-[2px] transition hover:brightness-125 pressed:bg-main-heavy md:h-28 md:w-[105px]"
-      >
-        {/* Clipped Background */}
-        <div
-          className={twJoin(
-            "clip-hexagon relative h-full w-full transition",
-            rarityBg(loadout.offering.rarity),
-          )}
+      <TooltipTrigger isOpen={offeringHovered} delay={0} closeDelay={0}>
+        <Button
+          onHoverChange={setOfferingHovered}
+          onPress={() => {
+            randomizeHandler(LoadoutPart.offering);
+          }}
+          className="clip-hexagon col-start-5 row-start-1 h-24 w-[84px] bg-main-light p-[2px] transition hover:brightness-125 pressed:bg-main-heavy md:h-28 md:w-[105px]"
         >
-          <LoadDetectImage
-            src={`${loadout?.offering.img}-144w.png`}
-            alt=""
-            fill
-            priority
-            className="pointer-events-none object-cover transition data-[loading=true]:opacity-0"
-          />
-        </div>
-      </Button>
+          {/* Clipped Background */}
+          <div
+            className={twJoin(
+              "clip-hexagon relative h-full w-full transition",
+              rarityBg(loadout.offering.rarity),
+            )}
+          >
+            <LoadDetectImage
+              src={`${loadout?.offering.img}-144w.png`}
+              alt=""
+              fill
+              priority
+              className="pointer-events-none object-cover transition data-[loading=true]:opacity-0"
+            />
+          </div>
+        </Button>
+        <Tooltip
+          placement="bottom"
+          offset={8}
+          className="border border-main-light bg-black/50 px-4 py-2 backdrop-blur-sm"
+        >
+          {loadout.offering.name}
+        </Tooltip>
+      </TooltipTrigger>
 
       {/* Perks */}
       <div className="col-start-3 col-end-[-1] row-start-3 flex gap-2">
-        {loadout.perks.map((perk) => (
-          <Button
+        {loadout.perks.map((perk, idx) => (
+          <TooltipTrigger
             key={perk.id}
-            onPress={() => {
-              randomizeHandler(LoadoutPart.perks);
-            }}
-            onHoverChange={setPerksHovered}
-            onPressChange={setPerksPressed}
-            className={twMerge(
-              "clip-diamond h-24 w-24 bg-main-light p-[2px] transition md:h-28 md:w-28",
-              perksHovered && "brightness-125",
-              perksPressed && "bg-main-heavy",
-            )}
+            isOpen={perksHoveredIdx === idx}
+            delay={0}
+            closeDelay={0}
           >
-            {/* Clipped Background */}
-            <div className="clip-diamond relative h-full w-full bg-gradient-to-b from-orange-800 to-orange-950">
-              <LoadDetectImage
-                src={`${perk.img}-112w.png`}
-                alt=""
-                fill
-                priority
-                className="pointer-events-none scale-110 object-cover transition data-[loading=true]:opacity-0"
-              />
-            </div>
-          </Button>
+            <Button
+              onHoverStart={() => {
+                setPerksHoveredIdx(idx);
+              }}
+              onHoverEnd={() => {
+                setPerksHoveredIdx(null);
+              }}
+              onPressStart={() => {
+                setPerksPressedIdx(idx);
+              }}
+              onPressEnd={() => {
+                setPerksPressedIdx(null);
+              }}
+              onPress={() => {
+                randomizeHandler(LoadoutPart.perks);
+              }}
+              className={twMerge(
+                "clip-diamond h-24 w-24 bg-main-light p-[2px] transition md:h-28 md:w-28",
+                perksHoveredIdx !== null && "brightness-125",
+                perksPressedIdx !== null && "bg-main-heavy",
+              )}
+            >
+              {/* Clipped Background */}
+              <div className="clip-diamond relative h-full w-full bg-gradient-to-b from-orange-800 to-orange-950">
+                <LoadDetectImage
+                  src={`${perk.img}-112w.png`}
+                  alt=""
+                  fill
+                  priority
+                  className="pointer-events-none scale-110 object-cover transition data-[loading=true]:opacity-0"
+                />
+              </div>
+            </Button>
+            <Tooltip
+              placement="bottom"
+              offset={8}
+              className="flex flex-col items-center gap-2 border border-main-light bg-black/50 px-4 py-2 backdrop-blur-sm"
+            >
+              <span>{loadout.perks[idx].name}</span>
+              <span className="text-sm opacity-75">
+                Hold shift to randomize a single perk
+              </span>
+            </Tooltip>
+          </TooltipTrigger>
         ))}
       </div>
 
