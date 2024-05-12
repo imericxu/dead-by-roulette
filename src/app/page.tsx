ed034bca487231052eed748fde2465bd753dfde2
Eric Xu <xeric.2002@gmail.com>
2024-05-12 19:44:01 +0800

    Add responsive UI to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,30 +8,36 @@ export default function Home(): ReactElement {
     <>
       <EffectPrefetchRoulette />
       <div className="col-span-full flex h-dvh min-h-min flex-col items-center gap-8 py-page">
-        <main className="flex w-40 grow flex-col items-center justify-center gap-8 justify-self-center text-center">
+        <main className="flex grow flex-col items-center justify-center gap-8 justify-self-center text-center">
           {/* Heading */}
           <h1 className="text-balance text-5xl font-bold uppercase">
-            Dead by Roulette
+            Dead by
+            <br className="md:hidden" />
+            <span className="hidden md:inline"> </span>
+            Roulette
           </h1>
 
           {/* CTA Links */}
-          <nav>
-            <ul className="flex flex-col items-stretch gap-4">
+          <nav className="w-full max-w-72 md:max-w-none">
+            <ul className="flex flex-col items-stretch gap-4 md:flex-row md:justify-between">
               {/* Killer Roulette */}
               <li>
-                <ButtonLink href="/roulette/killer" className="w-full">
+                <ButtonLink href="/roulette/killer" className="w-full md:w-36">
                   Killer
                 </ButtonLink>
               </li>
               {/* Survivor Roulette */}
               <li>
-                <ButtonLink href="/roulette/survivor" className="w-full">
+                <ButtonLink
+                  href="/roulette/survivor"
+                  className="w-full md:w-36"
+                >
                   Survivor
                 </ButtonLink>
               </li>
               {/* Random Roulette */}
               <li>
-                <ButtonLink href="/roulette" className="w-full">
+                <ButtonLink href="/roulette" className="w-full md:w-auto">
                   <LucideDices size={20} /> Pick for Me
                 </ButtonLink>
               </li>
@@ -39,7 +45,11 @@ export default function Home(): ReactElement {
           </nav>
 
           {/* Settings Link */}
-          <ButtonLink href="/settings?return=/" variant="ghost">
+          <ButtonLink
+            href="/settings?return=/"
+            variant="ghost"
+            className="w-full max-w-72 md:max-w-none"
+          >
             <LucideSettings size={20} /> Settings
           </ButtonLink>
         </main>
